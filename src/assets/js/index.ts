53a594dd182ebf67ce413c3f6eb9acb272bb3f8e
devmaffi <devmaffi550@gmail.com>
2022-05-03 18:50:55 +0300

    Implement swiper on the popular section
    Was provided swiper functional via swiper.js lib.
    
    Styled navigation buttons.
    
    Defined new rule at tsconfig file allows supporting node modules
    imports.
    
    Added swiper@8.1.4 prod dependency.

--- src/assets/js/index.ts
+++ src/assets/js/index.ts
@@ -1,12 +1,32 @@
 // Modules
 
 // modules
+import Swiper, { Navigation } from 'swiper'
+
 // @ts-ignore
 import { handleScroll } from '@modules'
 
 // styles
+// styles
+import 'swiper/css'
+import 'swiper/css/navigation'
 import '@sass/styles.sass'
 
 // Setup handlers
 
 handleScroll()
+
+// Define swiper
+
+new Swiper('.popular__container', {
+  modules: [Navigation],
+  loop: true,
+  spaceBetween: 32,
+  slidesPerView: 'auto',
+  grabCursor: true,
+  centeredSlides: true,
+  navigation: {
+    prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-button-next',
+  },
+})
